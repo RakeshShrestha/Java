6e25edf7c1aa7d942f6305b798401fd78b28929e
Rakesh Shrestha <rakesh.shrestha@gmail.com>
2020-11-08 17:10:59 +0545

    Update AppConfig.kt

--- javalintest/src/main/kotlin/app/config/AppConfig.kt
+++ javalintest/src/main/kotlin/app/config/AppConfig.kt
@@ -23,62 +23,62 @@ class AppConfig : KoinComponent {
     fun setup(): Javalin {
 	
         StandAloneContext.startKoin(
-                allModules,
-                KoinProperties(true, true)
+		allModules,
+		KoinProperties(true, true)
         )
 		
         val app = Javalin.create { 
-			it.server { createHttp2Server() }
-			it.addStaticFiles("/public")		
+		it.server { createHttp2Server() }
+		it.addStaticFiles("/public")		
         }.events {
             it.serverStopping {
                 StandAloneContext.stopKoin()
             }
         }.start()
-	
-		router.register(app)
-		
-		app.get("/") { it.result("Hello from rakesh") }
-		
-		return app
+
+	router.register(app)
+
+	app.get("/") { it.result("Hello from rakesh") }
+
+	return app
     }
 
     private fun createHttp2Server(): Server {
 
-		val alpn = ALPNServerConnectionFactory().apply {
-			defaultProtocol = "h2"
-		}
+	val alpn = ALPNServerConnectionFactory().apply {
+		defaultProtocol = "h2"
+	}
 
-		val sslContextFactory = SslContextFactory().apply {
-			keyStorePath = this::class.java.getResource("/keystore.jks").toExternalForm() // replace with your real keystore
-			setKeyStorePassword("password") // replace with your real password
-			cipherComparator = HTTP2Cipher.COMPARATOR
-			provider = "Conscrypt"
-		}
+	val sslContextFactory = SslContextFactory().apply {
+		keyStorePath = this::class.java.getResource("/keystore.jks").toExternalForm() // replace with your real keystore
+		setKeyStorePassword("password") // replace with your real password
+		cipherComparator = HTTP2Cipher.COMPARATOR
+		provider = "Conscrypt"
+	}
 
-		val ssl = SslConnectionFactory(sslContextFactory, alpn.protocol)
+	val ssl = SslConnectionFactory(sslContextFactory, alpn.protocol)
 
-		val httpsConfig = HttpConfiguration().apply {
-			sendServerVersion = false
-			secureScheme = "https"
-			securePort = 443
-			addCustomizer(SecureRequestCustomizer())
-		}
+	val httpsConfig = HttpConfiguration().apply {
+		sendServerVersion = false
+		secureScheme = "https"
+		securePort = 443
+		addCustomizer(SecureRequestCustomizer())
+	}
 
-		val http2 = HTTP2ServerConnectionFactory(httpsConfig)
+	val http2 = HTTP2ServerConnectionFactory(httpsConfig)
 
-		val fallback = HttpConnectionFactory(httpsConfig)
+	val fallback = HttpConnectionFactory(httpsConfig)
 
-		return Server().apply {
-			//HTTP/1.1 Connector
-			addConnector(ServerConnector(server).apply {
-				port = 80
-			})
-			// HTTP/2 Connector
-			addConnector(ServerConnector(server, sslContextFactory).apply {
-				port = 443
-			})
-		}
+	return Server().apply {
+		//HTTP/1.1 Connector
+		addConnector(ServerConnector(server).apply {
+			port = 80
+		})
+		// HTTP/2 Connector
+		addConnector(ServerConnector(server, sslContextFactory).apply {
+			port = 443
+		})
+	}
     }
 }
 

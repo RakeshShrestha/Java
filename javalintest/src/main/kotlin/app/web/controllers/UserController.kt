1e50e0a25f56621ec19c3a763f79050a5c7bd45e
RAKESHLAPTOP\Rakesh <rakesh.shrestha@gmail.com>
2020-11-08 17:15:34 +0545

    updates usercontroller

--- javalintest/src/main/kotlin/app/web/controllers/UserController.kt
+++ javalintest/src/main/kotlin/app/web/controllers/UserController.kt
@@ -3,19 +3,19 @@ package app.web.controllers
 import io.javalin.http.Context
 
 class UserController() {
-    fun login(ctx: Context) {
-	ctx.result("users login")
-    }
+	fun login(ctx: Context) {
+		ctx.result("users login")
+	}
 
-    fun register(ctx: Context) {
-	ctx.result("users register")
-    }
+	fun register(ctx: Context) {
+		ctx.result("users register")
+	}
 
-    fun getCurrent(ctx: Context) {
-	ctx.result("users getcurrent")
-    }
+	fun getCurrent(ctx: Context) {
+		ctx.result("users getcurrent")
+	}
 
-    fun update(ctx: Context) {
-	ctx.result("users update")
-    }
+	fun update(ctx: Context) {
+		ctx.result("users update")
+	}
 }

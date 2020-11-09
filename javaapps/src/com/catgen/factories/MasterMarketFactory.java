9147b121c5cc2fc34ace818a4e9e8782548f9559
RAKESHLAPTOP\Rakesh <rakesh.shrestha@gmail.com>
2020-11-09 07:08:22 +0545

    add copyright notice

--- javaapps/src/com/catgen/factories/MasterMarketFactory.java
+++ javaapps/src/com/catgen/factories/MasterMarketFactory.java
@@ -1,3 +1,12 @@
+# Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# Redistributions must retain the above copyright notice.
+
 package com.catgen.factories;
 
 import java.sql.Connection;
@@ -18,7 +27,7 @@ public class MasterMarketFactory
 		pstmt.executeUpdate();
 		
 		MasterMarketLoader masterMarketLoader = new MasterMarketLoader();
-		masterMarketLoader.LoadData(conn, "http://spreadsheets.google.com/feeds/cells/p2DIlEfXXvRGM1iKwllxSaw/od6/public/basic");
+		masterMarketLoader.LoadData(conn, "http://spreadsheets.google.com/feeds/cells/.............../od6/public/basic");
 	}
 	
 	private static void LoadMasterMarketFromResultSet(ResultSet rs, MasterMarket MasterMarket) throws SQLException
--- javaapps/src/com/catgen/loader/DoLoad.java
+++ javaapps/src/com/catgen/loader/DoLoad.java
@@ -1,20 +1,13 @@
+# Copyright Rakesh Shrestha (rakesh.shrestha@gmail.com)
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+# Redistributions must retain the above copyright notice.
+
 package com.catgen.loader;
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 
 public class DoLoad {
 
@@ -26,12 +19,12 @@ public class DoLoad {
 		/*MembersLoader membersLoader = new MembersLoader();
 		membersLoader.ClearExtras();
 		membersLoader.AddExtras("MarketID", "market2");
-		membersLoader.LoadData("http://spreadsheets.google.com/feeds/cells/p2DIlEfXXvRGop-VDBypYZg/od6/public/basic");
+		membersLoader.LoadData("http://spreadsheets.google.com/feeds/cells/.............../od6/public/basic");
 		
 		NetworkMarketInfoLoader networkMarketInfoLoader = new NetworkMarketInfoLoader();
 		networkMarketInfoLoader.ClearExtras();
 		networkMarketInfoLoader.AddExtras("MarketID", "market2");
-		networkMarketInfoLoader.LoadData("http://spreadsheets.google.com/feeds/cells/p2DIlEfXXvRGop-VDBypYZg/od7/public/basic");*/
+		networkMarketInfoLoader.LoadData("http://spreadsheets.google.com/feeds/cells/.............../od7/public/basic");*/
 	}
 }
 
